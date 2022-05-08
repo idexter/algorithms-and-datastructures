9a8d74cdb7206ff5bd604a05775608d36c173eda
Anton Kucherov <idexter.ru@gmail.com>
2022-05-08 03:43:32 +0300

    Fix inverted stack errors.

--- other/queue.go
+++ other/queue.go
@@ -20,13 +20,24 @@ func (q *QueueOnStack[T]) Dequeue() T {
 	for !q.OriginalStack.IsEmpty() {
 		q.InvertedStack.Push(q.OriginalStack.Pop())
 	}
-	return q.InvertedStack.Pop()
+	top := q.InvertedStack.Pop()
+	for !q.InvertedStack.IsEmpty() {
+		q.OriginalStack.Push(q.InvertedStack.Pop())
+	}
+	return top
 }
 
 func (q *QueueOnStack[T]) IsEmpty() bool {
-	return q.InvertedStack.IsEmpty()
+	return q.InvertedStack.IsEmpty() && q.OriginalStack.IsEmpty()
 }
 
 func (q *QueueOnStack[T]) Top() T {
-	return q.InvertedStack.Top()
+	for !q.OriginalStack.IsEmpty() {
+		q.InvertedStack.Push(q.OriginalStack.Pop())
+	}
+	top := q.InvertedStack.Top()
+	for !q.InvertedStack.IsEmpty() {
+		q.OriginalStack.Push(q.InvertedStack.Pop())
+	}
+	return top
 }
